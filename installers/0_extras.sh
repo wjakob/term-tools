7520363118e0d27e40358bd694e6bc9db6dc3292
Wenzel Jakob <wenzel@inf.ethz.ch>
2014-06-04 18:05:14 +0000

    fixed error in script

--- installers/0_extras.sh
+++ installers/0_extras.sh
@@ -2,9 +2,9 @@
 set -e
 
 if command -v apt-get >/dev/null 2>&1; then
-	sudo apt-get install -y zsh git-all vim less source-highlight \
+	sudo apt-get install -y zsh git vim less source-highlight \
 		libclang-3.4-dev clang-3.4 silversearcher-ag googlecl 
-if command -v brew >/dev/null 2>&1; then
+elif command -v brew >/dev/null 2>&1; then
 	brew install zsh vim source-highlight googlecl the_silver_searcher
 else
 	echo "ERROR: don't know how to install extras!"
