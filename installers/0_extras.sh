41391608183640e0261a84b1b43c1eb62b430a11
Wenzel Jakob <wenzel.jakob@epfl.ch>
2016-09-17 09:07:47 +0200

    fixed for redhat linux

--- installers/0_extras.sh
+++ installers/0_extras.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
-if command -v apt-get >/dev/null 2>&1; then
+if command -v yum >/dev/null 2>&1; then
+	sudo yum install -y zsh git vim less source-highlight \
+		 the_silver_searcher
+elif command -v apt-get >/dev/null 2>&1; then
 	sudo apt-get install -y zsh git vim less source-highlight \
-		libclang-3.4-dev clang-3.4 silversearcher-ag googlecl 
+		libclang-3.4-dev clang-3.4 silversearcher-ag 
 elif command -v brew >/dev/null 2>&1; then
 	brew install zsh vim source-highlight googlecl \
 		coreutils the_silver_searcher
