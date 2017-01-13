7aa58a3fd74813811317291eb612c0e84d38bc27
Wenzel Jakob <wenzel.jakob@epfl.ch>
2017-01-13 02:45:37 +0100

    added fzf to install script

--- installers/0_extras.sh
+++ installers/0_extras.sh
@@ -3,13 +3,13 @@ set -e
 
 if command -v yum >/dev/null 2>&1; then
 	sudo yum install -y zsh git vim less source-highlight \
-		 the_silver_searcher
+		 the_silver_searcher fzf
 elif command -v apt-get >/dev/null 2>&1; then
 	sudo apt-get install -y zsh git vim less source-highlight \
-		libclang-3.4-dev clang-3.4 silversearcher-ag 
+		libclang-3.4-dev clang-3.4 silversearcher-ag fzf
 elif command -v brew >/dev/null 2>&1; then
-	brew install zsh vim source-highlight googlecl \
-		coreutils the_silver_searcher
+	brew install zsh vim source-highlight \
+		coreutils the_silver_searcher fzf
 else
 	echo "ERROR: don't know how to install extras!"
 	exit
