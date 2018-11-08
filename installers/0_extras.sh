e4ec1b0e0cc9bd13053d22355e60b693136ed499
Wenzel Jakob <wenzel.jakob@epfl.ch>
2018-11-09 00:39:38 +0100

    colors in llvm backtraces

--- installers/0_extras.sh
+++ installers/0_extras.sh
@@ -9,7 +9,7 @@ elif command -v apt-get >/dev/null 2>&1; then
 		libclang-3.4-dev clang-3.4 silversearcher-ag fzf
 elif command -v brew >/dev/null 2>&1; then
 	brew install zsh vim source-highlight \
-		coreutils the_silver_searcher fzf
+		coreutils the_silver_searcher
 else
 	echo "ERROR: don't know how to install extras!"
 	exit
