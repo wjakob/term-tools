ac23eafd491d3ed762b80f0e6108276f9061db9e
Wenzel Jakob <wenzel.jakob@epfl.ch>
2020-02-02 01:44:50 +0100

    major vimrc redesign

--- installers/vim.sh
+++ installers/vim.sh
@@ -18,9 +18,8 @@ fi
 
 # Install dotfiles (this will fail it already exists so we are safe)
 ln $@ -s $TERM_TOOLS/config/vimrc ~/.vimrc
-ln $@ -s $TERM_TOOLS/config/gvimrc ~/.gvimrc
 
-mkdir -p ~/.vim/bundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-vim +PluginInstall +qall
+vim +PlugInstall +qall
