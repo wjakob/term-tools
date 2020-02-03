fedac80ae2a92e780b88761ad65f3f74b3b2afd2
Wenzel Jakob <wenzel.jakob@epfl.ch>
2020-02-03 03:54:25 +0100

    bare bones zsh

--- config/shrc.sh
+++ config/shrc.sh
@@ -1,19 +1,25 @@
-# TERMINAL CONFIG
-# source from ~/.bashrc or ~/.zshrc
-
-# directory containing these tools
 if [ "$ZSH_VERSION" ]; then
     export TERM_TOOLS=$(dirname $(dirname $0:A))
 else
     export TERM_TOOLS="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 fi
 
+# Don't pause VIM upon Ctrl-S shortcut..
+stty stop undef
+stty -ixon
+
 # vim by default
 export EDITOR="vim"
 
-# store more shell history
-export HISTSIZE=1000000
+# Store extensive history
+export SAVEHIST=10000
+export HISTSIZE=50000
 export HISTFILESIZE=1000000
+export HISTFILE=$TERM_TOOLS/history
+
+# zstyle :compinstall filename '/Users/wjakob/.zshrc'
+# autoload -Uz compinit
+# compinit
 
 # enable core dumps
 ulimit -c unlimited 2>/dev/null
@@ -23,54 +29,105 @@ if [[ "$TERM" == "xterm" ]]; then
 	export TERM="xterm-256color"
 fi
 
-# syntax highlighting in less
-if [ -e /usr/share/source-highlight/src-hilite-lesspipe.sh ]; then
-	export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
-	export LESS=' -R '
-elif [ -e /usr/local/bin/src-hilite-lesspipe.sh ]; then
-	export LESSOPEN="| /usr/local/bin/src-hilite-lesspipe.sh %s"
-	export LESS=' -R '
-fi
-
-
-[[ -s /usr/share/autojump/autojump.sh ]] && source /usr/share/autojump/autojump.sh
-[[ -s /usr/local/etc/autojump.sh ]] && source /usr/local/etc/autojump.sh
-
-# send ctrl-s to vim
-# see http://unix.stackexchange.com/questions/12107/how-to-unfreeze-after-accidentally-pressing-ctrl-s-in-a-terminal
-stty stop undef
-stty -ixon
-
 if uname | grep Darwin > /dev/null; then
 	# Mac specific commands
-
-	# ls with a 1-second timeout
-	function ls_safe {
-		$TERM_TOOLS/config/timeout3.sh -t 1 ls -G
-	}
 
 	# Also bind find to 'gfind' (install via brew)
 	alias find=gfind
 
 	# top: sort by cpu
 	alias top="top -o cpu"
+
+    export BROWSER=/Applications/Firefox.app/Contents/MacOS/firefox-bin
+
+	# ls with a 1-second timeout
+	function ls_safe {
+		$TERM_TOOLS/config/timeout3.sh -t 1 ls -G
+	}
+
+    function skim () {
+        /Applications/Skim.app/Contents/MacOS/Skim $@ &> /dev/null &
+    }
+
+    # Don't include "_something" AppleDouble files in tar files, etc.
+    export COPYFILE_DISABLE=1
 else
 	alias gnome-open=gvfs-open
-	alias gqview=geeqie
 	alias open=gvfs-open
 	alias gdate=date
 
 	function ls_safe {
 		$TERM_TOOLS/config/timeout3.sh -t 1 ls --color=auto
 	}
+
+    # Support High-DPI in Qt5 apps
+    export QT_AUTO_SCREEN_SCALE_FACTOR=1
+
+    # GNU ls: don't wrap entries with spaces into quotes
+    export QUOTING_STYLE=literal
 fi
 
-# ZSH-SPECIFIC CONFIG
+# Don't make core dumps by default
+ulimit -c 0
+
+# Colors in LS
+alias ls='ls -G'
+
+# Locale
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+# Ninja: fancy status message
+export NINJA_STATUS="[%u/%r/%f] "
+
+# Generate a compile_commands.json file
+alias cmake='cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1'
+
+# Convenience flags to switch between release and debug mode
+alias cmake_deb='cmake $@ -GNinja -DCMAKE_BUILD_TYPE=DEBUG'
+alias cmake_rel='cmake $@ -GNinja -DCMAKE_BUILD_TYPE=RELEASE'
+
+# CMake aliases for various compilers
+alias cmake_gcc='cmake $@ -GNinja -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc'
+alias cmake_clang='cmake $@ -GNinja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++'
+alias cmake_clean='rm -rf CMakeCache.txt CMakeFiles'
+
+# Other aliases
+alias grpe=grep
+alias gdb='gdb -quiet'
+alias n=ninja
+alias python=python3
+alias ipython=ipython3
+alias pip=pip3
+alias octave='octave --no-gui-libs'
+alias pdb='python3 -u -m pdb -c continue'
+
+# zsh-specific configuration
 if [ "$ZSH_VERSION" ]; then
-	ZSH=$HOME/.oh-my-zsh
-	plugins=(zsh-syntax-highlighting)
-	ZSH_THEME="wjakob"
-	source $ZSH/oh-my-zsh.sh
+    autoload -U history-search-end
+    zle -N history-beginning-search-backward-end history-search-end
+    zle -N history-beginning-search-forward-end history-search-end
+
+    bindkey $terminfo[kbs]   backward-delete-char           # Backspace
+    bindkey $terminfo[kdch1] delete-char                    # Delete
+    bindkey '\e[3~'          delete-char
+    bindkey $terminfo[khome] beginning-of-line              # Home
+    bindkey "^A"             beginning-of-line
+    bindkey $terminfo[kend]  end-of-line                    # End
+    bindkey "^E"             end-of-line
+    bindkey $terminfo[kcuu1] history-beginning-search-backward-end # Down
+    bindkey "^[[A"           history-beginning-search-backward-end # Down
+    bindkey $terminfo[kcud1] history-beginning-search-forward-end  # Up
+    bindkey "^[[B"           history-beginning-search-forward-end  # Up
+
+    bindkey $terminfo[kcub1] backward-char                  # Left
+    bindkey $terminfo[kcuf1] forward-char                   # Right
+
+    bindkey -M vicmd $terminfo[khome] beginning-of-line
+    bindkey -M vicmd "^A"             beginning-of-line
+    bindkey -M vicmd $terminfo[kend]  end-of-line           # End
+    bindkey -M vicmd "^E"             end-of-line
+    bindkey -M vicmd '\e[3~'          delete-char
 
 	# ls after every cd
 	function chpwd() {
@@ -78,56 +135,72 @@ if [ "$ZSH_VERSION" ]; then
 		ls_safe
 	}
 
-	# vim keybindings for zsh
-	bindkey '\e[3~' delete-char
-
 	# no error if glob fails to expand (scp fix)
 	unsetopt nomatch
 
-	# Automatically escape wildcards in 'scp' and 'rsync' commands
-	__remote_commands=(scp rsync)
+	# Automatically escape wildcards in 'scp', 'rsync', and any command with http/ftp args
 	autoload -U url-quote-magic
 	zle -N self-insert url-quote-magic
-	zstyle -e :urlglobber url-other-schema '[[ $__remote_commands[(i)$words[1]] -le ${#__remote_commands} ]] && reply=("*") || reply=(http https ftp)'
+	zstyle -e :urlglobber url-other-schema \ '[[ $words[1] == scp ]] && reply=("*") || reply=(http https ftp)'
 
-	# Enable VIM mode, adjust delays
+	# VI mode: enable
 	export KEYTIMEOUT=1
 	bindkey -v
 
-	# VIM mode: visualize the current editing mode
-	vim_ins_mode="[INS]"
-	vim_cmd_mode="[CMD]"
-	vim_mode=$vim_ins_mode
-	function zle-keymap-select {
-		vim_mode="${${KEYMAP/vicmd/${vim_cmd_mode}}/(main|viins)/${vim_ins_mode}}"
-		zle reset-prompt
-	}
-	function zle-line-finish {
-		vim_mode=$vim_ins_mode
-	}
-	zle -N zle-keymap-select
-	zle -N zle-line-finish
-	RPROMPT='${vim_mode}'
+    # VI mode: allow deleting beyond original insertion
+    bindkey -M viins '^?' backward-delete-char
+    bindkey -M viins '^H' backward-delete-char
 
-	autoload up-line-or-history
-	autoload down-line-or-history
+    # Change cursor shape for different vi modes.
+    function zle-keymap-select {
+      if [[ ${KEYMAP} == vicmd ]] ||
+         [[ $1 = 'block' ]]; then
+        echo -ne '\e[1 q'
+
+      elif [[ ${KEYMAP} == main ]] ||
+           [[ ${KEYMAP} == viins ]] ||
+           [[ ${KEYMAP} = '' ]] ||
+           [[ $1 = 'beam' ]]; then
+        echo -ne '\e[5 q'
+      fi
+    }
+    zle -N zle-keymap-select
+
+    _fix_cursor() {
+       echo -ne '\e[5 q'
+    }
+    precmd_functions+=(_fix_cursor)
 
 	# Perform a prefix history search when navigating previous commands
 	bindkey -M vicmd 'k' history-beginning-search-backward
 	bindkey -M vicmd 'j' history-beginning-search-forward
-	bindkey -M vicmd 'v' edit-command-line
-	source $TERM_TOOLS/config/zsh-vi-search.zsh
+
+    autoload -U edit-command-line
+	zle -N edit-command-line
+	bindkey -M vicmd V edit-command-line
+
+	# Share history between multiple terminals
+    setopt inc_append_history
+    setopt share_history
+
+    # Completions
+    zstyle :compinstall filename '~/.zshrc'
+    autoload -Uz compinit
+    compinit -d $TERM_TOOLS/zcompdump-$ZSH_VERSION
+
+    # A colorful prompt
+    PROMPT="%{%f%k%b%}
+%{%K{black}%B%F{green}%}%n%{%B%F{blue}%}@%{%B%F{cyan}%}%m%{%B%F{green}%} %{%b%F{yellow}%K{black}%}%~%{%B%F{green}%}%E%{%b%}
+%{%K{black}%}%{%K{black}%} %#%{%f%k%b%} "
 fi
 
-# BASH-SPECIFIC CONFIG
+# bash-specific configuration
 if [ "$BASH_VERSION" ]; then
 	# ls after every cd
 	function cd()  {
 		 builtin cd "$@" && ls_safe
 	}
 
-	# Custom terminal: blue path and yellow git branch
-	#export PROMPT_DIRTRIM=2  # uncomment to trim to 2 directories
 	DEFAULT_COLOR="\[\e[0m\]"
 	PS1_COLOR="\[\e[34m\]"
 	TITLEBAR="\[\e]0;\h \w\007\]"
@@ -153,22 +226,3 @@ if [ "$BASH_VERSION" ]; then
 	alias mkdir='nocorrect mkdir '
 fi
 
-function pw { $TERM_TOOLS/config/make-password.pl -p -a $1 ~/.pwdata ; }
-
-function get_timezone {
-	if [ -z "$TZNAME" ]; then
-		if [ -f /etc/timezone ]; then
-			TZNAME=`cat /etc/timezone`
-		elif [ -h /etc/localtime ]; then
-			TZNAME=`readlink /etc/localtime | sed "s/\/usr\/share\/zoneinfo\///"`
-		fi
-	fi
-}
-
-# Convenience to convert between EST/PST/CE(S)T
-function from_pst { gdate --date="TZ=\"US/Pacific\"    $@" }
-function from_est { gdate --date="TZ=\"US/Eastern\"    $@" }
-function from_cet { gdate --date="TZ=\"Europe/Berlin\" $@" }
-function to_pst   { get_timezone; TZ="US/Pacific"    gdate --date="TZ=\"$TZNAME\" $@" }
-function to_est   { get_timezone; TZ="US/Eastern"    gdate --date="TZ=\"$TZNAME\" $@" }
-function to_cet   { get_timezone; TZ="Europe/Berlin" gdate --date="TZ=\"$TZNAME\" $@" }
--- installers/zsh.sh
+++ installers/zsh.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 set -e
 
-ZSH="$HOME/.oh-my-zsh"
-
 if [ -z "$TERM_TOOLS" ]; then
     echo "TERM_TOOLS environment variable is not set!"
     exit 1
 fi
 
-if [ ! -d "$ZSH" ]; then
-	git clone https://github.com/robbyrussell/oh-my-zsh.git $ZSH
-fi
-
-if [ ! -e "$ZSH/custom/themes/wjakob.zsh-theme" ]; then
-	mkdir -p $ZSH/themes
-	ln $@ -s $TERM_TOOLS/config/wjakob.zsh-theme $ZSH/themes/wjakob.zsh-theme
-fi
-
-if [ ! -d "$ZSH/custom/plugins/zsh-syntax-highlighting" ]; then
-	mkdir -p $ZSH/custom/plugins
-	git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting
-fi
-
 if [ -e ~/.zshrc ]; then
-	if [ "$1" == "-f" ]; then
-		cp $TERM_TOOLS/config/zshrc ~/.zshrc
-	else
-		echo "Error: .zshrc exists.  Move or delete ~/.zshrc"
-		exit 1
-	fi
+    echo "Error: .zshrc exists.  Move or delete ~/.zshrc"
+    exit 1
 else
 	cp $TERM_TOOLS/config/zshrc ~/.zshrc
 fi
