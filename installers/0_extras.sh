3329b44bab26d35e6f261b13537532a7eac73033
Wenzel Jakob <wenzel.jakob@epfl.ch>
2020-02-03 16:21:19 +0100

    tmux integration

--- installers/0_extras.sh
+++ installers/0_extras.sh
@@ -5,10 +5,10 @@ if command -v apt >/dev/null 2>&1; then
     sudo apt install -y zsh git vim less source-highlight \
         clang-9 clangd-9 lldb-9 clang-tools-9 clang clangd lldb clang-tools \
         libc++abi1-9  libc++abi-9-dev libc++1-9 libc++-9-dev \
-        silversearcher-ag fzf ninja-build
+        silversearcher-ag fzf ninja-build tmux
 elif command -v brew >/dev/null 2>&1; then
     brew install zsh vim source-highlight lesspipe llvm \
-        coreutils the_silver_searcher
+        coreutils the_silver_searcher tmux
 else
     echo "ERROR: don't know how to install extras!"
     exit
