0fec8ca8eeb3eb526d94b3cf7d736d2359ab93f5
Wenzel Jakob <wenzel.jakob@epfl.ch>
2020-02-04 17:36:01 +0100

    fzf refinements

--- config/shrc.sh
+++ config/shrc.sh
@@ -82,6 +82,16 @@ elif [ -e /usr/local/bin/src-hilite-lesspipe.sh ]; then
    export LESS=' -R '
 fi
 
+if type "fd" > /dev/null; then
+    export FZF_DEFAULT_COMMAND="fd --type f"
+else
+    export FZF_DEFAULT_COMMAND="fdfind --type f"
+    alias fd=fdfind
+fi
+
+export FZF_DEFAULT_OPTS="--extended"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
 # Locale
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
@@ -229,6 +239,7 @@ if [ "$ZSH_VERSION" ]; then
     done
 
     source $TERM_TOOLS/config/prompt.zsh
+    source $TERM_TOOLS/config/fzf.zsh
 fi
 
 # bash-specific configuration
--- config/wjakob.zsh-theme
+++
@@ -1,5 +0,0 @@
-setopt prompt_subst
- 
-PROMPT='%{%f%k%b%}
-%{%K{black}%B%F{green}%}%n%{%B%F{blue}%}@%{%B%F{cyan}%}%m%{%B%F{green}%} %{%b%F{yellow}%K{black}%}%~%{%B%F{green}%}%E%{%b%}
-%{%K{black}%}%{%K{black}%} %#%{%f%k%b%} '
