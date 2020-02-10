d134dbe69b7a871914c77091f3627915c08f2eb9
Wenzel Jakob <wenzel.jakob@epfl.ch>
2020-02-10 23:56:51 +0100

    pdflatex: always be non-interactive

--- config/shrc.sh
+++ config/shrc.sh
@@ -99,6 +99,9 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
+# TeX
+alias pdflatex='pdflatex -interaction=nonstopmode -file-line-error -halt-on-error'
+
 # Ninja: fancy status message
 export NINJA_STATUS="[%u/%r/%f] "
 
